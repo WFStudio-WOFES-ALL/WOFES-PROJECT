18e384e011f2700e3b49f3c144afd6c0db7d8035
Wolfgang <wolfgang110.kurenkov@gmail.com>
2026-03-31 23:34:29 +0300

    Update file

--- boot.asm
+++ boot.asm
@@ -3,6 +3,14 @@
 global start
 extern kernel_main
 
+; Multiboot header (обязательно для QEMU)
+section .multiboot
+align 4
+    dd 0x1BADB002          ; магическое число
+    dd 0x03                ; флаги
+    dd -(0x1BADB002 + 0x03); контрольная сумма
+
+section .text
 start:
     mov esp, stack_top
     call kernel_main
